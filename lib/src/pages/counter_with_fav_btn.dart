21fe7022e35c1ca864bf8a98049b9a0383703d79
bismatariq <74257320+bismatariq98@users.noreply.github.com>
2020-11-22 23:28:59 -0800

    user

--- lib/src/pages/counter_with_fav_btn.dart
+++ lib/src/pages/counter_with_fav_btn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 
 import 'cart_counter.dart';
@@ -14,6 +15,25 @@ class CounterWithFavBtn extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         CartCounter(),
+        Padding(
+          padding: EdgeInsets.only(right: 20),
+          child: SmoothStarRating(
+            rating: 2.0,
+            isReadOnly: false,
+            size: 15,
+            color: Colors.red,
+            filledIconData: Icons.star,
+            halfFilledIconData: Icons.star_half,
+            defaultIconData: Icons.star_border,
+            starCount: 5,
+            allowHalfRating: true,
+            spacing: 2.0,
+            onRated: (value) {
+              print("rating value -> $value");
+              // print("rating value dd -> ${value.truncate()}");
+            },
+          ),
+        )
         // IconButton(
         //   icon: Icon(Icons.favorite),
         //   onPressed: () {},
