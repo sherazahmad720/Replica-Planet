bd2214526aff62746c0afa02ce72a89199c36546
bismatariq <74257320+bismatariq98@users.noreply.github.com>
2020-11-23 02:21:42 -0800

    newone

--- lib/src/pages/Homepage.dart
+++ lib/src/pages/Homepage.dart
@@ -2,6 +2,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:karobar/main.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:karobar/src/pages/Categories.dart';
 import 'package:karobar/src/pages/SeeMore.dart';
 import 'package:karobar/src/widgets/CustomFieldSearch.dart';
 import 'package:karobar/src/widgets/CustomTextField.dart';
@@ -12,6 +13,7 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'ProductDetails.dart';
+import 'package:karobar/src/pages/CategoryPage.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -189,109 +191,121 @@ class _HomePageState extends State<HomePage> {
                             prodPrice: products[i].price.toString(),
                             proddescription: products[i].description));
                       },
-                      child: Container(
-                        height: 100,
-                        width: 250,
-                        child: Card(
-                          color: Color(0XFF3D81AF),
-                          elevation: 10.0,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Container(
-                                height: 100,
-                                width: 100,
-                                child: Hero(
-                                    tag: '${products[i].id}',
-                                    child: Text('${products[i].title}')
-                                    // Image.asset(products[i].image),
-                                    ),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 12),
-                                child: Row(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ProductDetails(
+                                        product: products[i],
+                                      )));
+                        },
+                        child: Container(
+                          height: 100,
+                          width: 250,
+                          child: Card(
+                            color: Color(0XFF3D81AF),
+                            elevation: 10.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 100,
+                                  width: 100,
+                                  child: Hero(
+                                      tag: '${products[i].id}',
+                                      child: Text('${products[i].title}')
+                                      // Image.asset(products[i].image),
+                                      ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 12),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        flex: 1,
+                                        child: Container(
+                                            height: 50,
+                                            width: 100,
+                                            child: Padding(
+                                              padding: EdgeInsets.only(top: 10),
+                                              child: Text(
+                                                  "${products[i].title}",
+                                                  style: TextStyle(
+                                                      fontSize: 20,
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                            )),
+                                      ),
+                                      Expanded(
+                                        flex: 1,
+                                        child: SmoothStarRating(
+                                          rating: rating,
+                                          isReadOnly: false,
+                                          size: 15,
+                                          filledIconData: Icons.star,
+                                          halfFilledIconData: Icons.star_half,
+                                          defaultIconData: Icons.star_border,
+                                          starCount: 5,
+                                          allowHalfRating: true,
+                                          spacing: 2.0,
+                                          onRated: (value) {
+                                            print("rating value -> $value");
+                                            // print("rating value dd -> ${value.truncate()}");
+                                          },
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Text(
+                                    "${products[i].description}",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Row(
                                   mainAxisAlignment:
                                       MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                          height: 50,
-                                          width: 100,
-                                          child: Padding(
-                                            padding: EdgeInsets.only(top: 10),
-                                            child: Text("${products[i].title}",
-                                                style: TextStyle(
-                                                    fontSize: 20,
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                          )),
+                                    Text(
+                                      "\$${products[i].price}",
+                                      style: TextStyle(fontSize: 22),
                                     ),
-                                    Expanded(
-                                      flex: 1,
-                                      child: SmoothStarRating(
-                                        rating: rating,
-                                        isReadOnly: false,
-                                        size: 15,
-                                        filledIconData: Icons.star,
-                                        halfFilledIconData: Icons.star_half,
-                                        defaultIconData: Icons.star_border,
-                                        starCount: 5,
-                                        allowHalfRating: true,
-                                        spacing: 2.0,
-                                        onRated: (value) {
-                                          print("rating value -> $value");
-                                          // print("rating value dd -> ${value.truncate()}");
-                                        },
-                                      ),
-                                    )
+                                    Text(
+                                      "\$${products[i].price}",
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          decoration:
+                                              TextDecoration.lineThrough),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.favorite),
+                                      onPressed: () {
+                                        return null;
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.shopping_cart_outlined),
+                                      onPressed: () {
+                                        return null;
+                                      },
+                                    ),
                                   ],
                                 ),
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Text(
-                                  "${products[i].description}",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "\$${products[i].price}",
-                                    style: TextStyle(fontSize: 22),
-                                  ),
-                                  Text(
-                                    "\$${products[i].price}",
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        decoration: TextDecoration.lineThrough),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.favorite),
-                                    onPressed: () {
-                                      return null;
-                                    },
-                                  ),
-                                  IconButton(
-                                    icon: Icon(Icons.shopping_cart_outlined),
-                                    onPressed: () {
-                                      return null;
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
